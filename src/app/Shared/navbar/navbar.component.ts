67d7f86d7289a34dda7c5b4b80f1f5c45e9dc4c9
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2023-06-10 22:31:03 -0300

    Added logout functionality

--- src/app/Shared/navbar/navbar.component.ts
+++ src/app/Shared/navbar/navbar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private route: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  readToken(): boolean {
+    const token = localStorage.getItem('token');
+    return token !== null;
   }
 
+  logout(): void {
+    console.log('click');
+    localStorage.removeItem('token');
+    this.route.navigate(['']);
+  }
 }
