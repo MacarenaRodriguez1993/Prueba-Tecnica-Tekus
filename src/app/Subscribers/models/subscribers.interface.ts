59f86917f7366020942913575706776a9da18e34
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2023-06-11 11:39:32 -0300

    create inferface subscribers

---
+++ src/app/Subscribers/models/subscribers.interface.ts
@@ -0,0 +1,23 @@
+export interface Subscriber {
+  SystemId: string | null;
+  Area: string;
+  PublicId: number;
+  CountryCode: string;
+  CountryName: string;
+  Name: string;
+  Email: string;
+  JobTitle: string;
+  PhoneNumber: string;
+  PhoneCode: string;
+  PhoneCodeAndNumber: string;
+  LastActivityUtc: string;
+  LastActivity: string;
+  SubscriptionDate: string;
+  SubscriptionMethod: number;
+  SubscriptionState: number;
+  SubscriptionStateDescription: string;
+  Topics: [Array<string>];
+  Activity: string;
+  ConnectionState: number;
+  Id: number;
+}
