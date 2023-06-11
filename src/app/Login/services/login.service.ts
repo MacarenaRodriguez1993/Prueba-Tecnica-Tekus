f3fae946d9a3e96da3974ded44525934214830a7
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2023-06-10 21:00:16 -0300

    Implemented service for API login calls

--- src/app/Login/services/login.service.ts
+++ src/app/Login/services/login.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable, map, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Login } from '../models/login.interface';
+import { catchError } from 'rxjs/operators';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  login(credencials: Login): Observable<any> {
+    return this.http
+      .post(`${environment.API_URL}account/login`, credencials)
+      .pipe(
+        map((res: any) => {
+          return res;
+        }),
+        catchError((error: HttpErrorResponse) => {
+          return throwError(error.error);
+        })
+      );
+  }
 }
